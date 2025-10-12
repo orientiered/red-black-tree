f19e9ef4f4f6e9575126f69410ddc65c36edb012
crefr <fedor30122005@gmail.com>
2025-10-12 20:05:45 +0300

    Fix compiler warnings
    Remove shadowing declarations and delete copying and assigning.

--- include/rbtree.hpp
+++ include/rbtree.hpp
@@ -14,30 +14,33 @@ class Tree {
     };
 
     struct Node {
-        T key;
+        T key_;
 
-        Node *parent = nullptr;
-        Node *left   = nullptr;
-        Node *right  = nullptr;
+        Node *parent_ = nullptr;
+        Node *left_   = nullptr;
+        Node *right_  = nullptr;
 
-        Color color = Color::black;
+        Color color_ = Color::black;
 
-        Node(const T& key, Color color = Color::black) : key(key), color(color) {}
-        Node(const T& key, Node * parent, Color color = Color::black) : key(key), parent(parent), color(color) {}
+        Node(const T& key, Color color = Color::black) : key_(key), color_(color) {}
+        Node(const T& key, Node * parent, Color color = Color::black) : key_(key), parent_(parent), color_(color) {}
 
-        void make_nil() {
-            parent = this;
-            right  = this;
-            left   = this;
-            color = Color::black;
-        }
+        Node(const Node& rhs) = delete;
+        Node &operator=(const Node& rhs) = delete;
 
         ~Node() {
             //TODO: find a better solution
-            if (left != this)
-                delete left;
-            if (right != this)
-                delete right;
+            if (left_ != this)
+                delete left_;
+            if (right_ != this)
+                delete right_;
+        }
+
+        void make_nil() {
+            parent_ = this;
+            right_  = this;
+            left_   = this;
+            color_ = Color::black;
         }
 
         /* DEBUG FUNCTIONS HERE */
@@ -47,16 +50,16 @@ class Tree {
                 indent_str += "\t";
 
             stream << indent_str << "Node " << this << ":\n";
-            stream << indent_str << "\t" << "color = " << ((color == Color::black) ? "black" : "red") << "\n";
-            stream << indent_str << "\t" << "key = " << key << "\n";
-            stream << indent_str << "\t" << "parent " << parent << "\n";
-            stream << indent_str << "\t" << "left   " << left   << "\n";
-            stream << indent_str << "\t" << "right  " << right  << "\n";
+            stream << indent_str << "\t" << "color = " << ((color_ == Color::black) ? "black" : "red") << "\n";
+            stream << indent_str << "\t" << "key = " << key_ << "\n";
+            stream << indent_str << "\t" << "parent " << parent_ << "\n";
+            stream << indent_str << "\t" << "left   " << left_   << "\n";
+            stream << indent_str << "\t" << "right  " << right_  << "\n";
         }
     };
 
-    Node *root = nullptr;
-    Node *tree_nil = nullptr;
+    Node *root_ = nullptr;
+    Node *tree_nil_ = nullptr;
 
     /*
           x               y
@@ -73,90 +76,93 @@ class Tree {
     void right_rotate(Node *node);
 
     void insert_fixup(Node *node) {
-        while (node->parent->color == Color::red)
+        while (node->parent_->color == Color::red)
         {
-            if (node->parent == node->parent->parent->left)
+            if (node->parent_ == node->parent_->parent->left)
             {
-                Node *y = node->parent->parent->right;
-                if (y->color == Color::red) {
-                    node->parent->color = Color::black;
-                    y->color = Color::black;
-                    node->parent->parent->color = Color::red;
-                    node = node->parent->parent;
+                Node *y = node->parent_->parent->right;
+                if (y->color_ == Color::red) {
+                    node->parent_->color = Color::black;
+                    y->color_ = Color::black;
+                    node->parent_->parent->color = Color::red;
+                    node = node->parent_->parent;
                 } else {
-                    if (node == node->parent->right) {
-                        node = node->parent;
+                    if (node == node->parent_->right) {
+                        node = node->parent_;
                         left_rotate(node);
                     }
-                    node->parent->color = Color::black;
-                    node->parent->parent->color = Color::red;
-                    right_rotate(node->parent->parent);
+                    node->parent_->color = Color::black;
+                    node->parent_->parent->color = Color::red;
+                    right_rotate(node->parent_->parent);
                 }
             } else {
-                Node *y = node->parent->parent->left;
-                if (y->color == Color::red) {
-                    node->parent->color = Color::black;
-                    y->color = Color::black;
-                    node->parent->parent->color = Color::red;
-                    node = node->parent->parent;
+                Node *y = node->parent_->parent->left;
+                if (y->color_ == Color::red) {
+                    node->parent_->color = Color::black;
+                    y->color_ = Color::black;
+                    node->parent_->parent->color = Color::red;
+                    node = node->parent_->parent;
                 } else {
-                    if (node == node->parent->left) {
-                        node = node->parent;
+                    if (node == node->parent_->left) {
+                        node = node->parent_;
                         right_rotate(node);
                     }
-                    node->parent->color = Color::black;
-                    node->parent->parent->color = Color::red;
-                    left_rotate(node->parent->parent);
+                    node->parent_->color = Color::black;
+                    node->parent_->parent->color = Color::red;
+                    left_rotate(node->parent_->parent);
                 }
             }
         }
 
-        root->color = Color::BLACK;
+        root_->color_ = Color::BLACK;
     }
 public:
     using iterator = Node *;
 
     Tree() {
-        tree_nil = new Node(T());
-        tree_nil->make_nil();
+        tree_nil_ = new Node(T());
+        tree_nil_->make_nil();
     }
     ~Tree() {
-        delete root;
-        delete tree_nil;
+        delete root_;
+        delete tree_nil_;
     }
+    Tree(const Tree& rhs) = delete;
+    Tree &operator=(const Tree& rhs) = delete;
+
 
     void insert(const T& key) {
         Node *new_node = new Node(key, Color::red);
 
-        Node *prev = tree_nil;
-        Node *cur = root;
+        Node *prev = tree_nil_;
+        Node *cur = root_;
 
-        while (cur != tree_nil) {
+        while (cur != tree_nil_) {
             prev = cur;
-            if (new_node->key < cur->key) {
-                cur = cur->left;
+            if (new_node->key_ < cur->key_) {
+                cur = cur->left_;
             } else {
-                cur = cur->right;
+                cur = cur->right_;
             }
         }
 
-        // cur is leaf, prev is it's parent
-        new_node->parent = prev;
+        // cur is leaf, prev is its parent
+        new_node->parent_ = prev;
         // empty tree
-        if (prev == tree_nil) {
-            root = new_node;
-        // othwerise putting new node to the correct subtree
-        } else if (new_node->key < prev->key) {
-            prev->left = new_node;
+        if (prev == tree_nil_) {
+            root_ = new_node;
+        // othwerise put new node to the correct subtree
+        } else if (new_node->key_ < prev->key_) {
+            prev->left_ = new_node;
         } else {
-            prev->right = new_node;
+            prev->right_ = new_node;
         }
 
         insert_fixup(new_node);
     }
 
     iterator get_root() const {
-        return root;
+        return root_;
     }
 
     iterator lower_bound(const T& key) const;
@@ -175,16 +181,16 @@ void Tree<T>::print_sorted(std::ostream stream, iterator node) const {
     if (node == nullptr)
         return;
 
-    print_sorted(stream,  node->left);
+    print_sorted(stream,  node->left_);
     stream << node << " ";
-    print_sorted(stream, node->right);
+    print_sorted(stream, node->right_);
 }
 
 template <typename T>
 void Tree<T>::print_debug(std::ostream stream, iterator node, int indent) const {
     node->print_debug(stream, indent);
-    print_debug(stream, node->left, indent + 1);
-    print_debug(stream, node->right, indent + 1);
+    print_debug(stream, node->left_, indent + 1);
+    print_debug(stream, node->right_, indent + 1);
 }
 
 template <typename T>
@@ -195,35 +201,35 @@ void Tree<T>::left_rotate(Node *node) {
       b   c            a   b
     */
     assert(node);
-    if (node->right == tree_nil) return;
+    if (node->right_ == tree_nil_) return;
 
     Node *&x = node;
 
-    Node *y = x->right;
+    Node *y = x->right_;
     // (b) left y subtree -> right x subtree
-    x->right = y->left;
+    x->right_ = y->left_;
 
     // b parent
-    if (y->left != tree_nil) {
-        y->left->parent = x;
+    if (y->left_ != tree_nil_) {
+        y->left_->parent = x;
     }
 
     // y parent
-    y->parent = x->parent;
+    y->parent_ = x->parent_;
 
     // x was tree root
-    if (x->parent == tree_nil) {
-        root = y;
+    if (x->parent_ == tree_nil_) {
+        root_ = y;
     // x was left subtree
-    } else if (x == x->parent->left) {
-        x->parent->left = y;
+    } else if (x == x->parent_->left) {
+        x->parent_->left = y;
     // x was right subtree
     } else {
-        x->parent->right = y;
+        x->parent_->right = y;
     }
 
-    y->left = x;
-    x->parent = y;
+    y->left_ = x;
+    x->parent_ = y;
 }
 
 template <typename T>
@@ -234,35 +240,35 @@ void Tree<T>::right_rotate(Node *node) {
     a   b           b   c
     */
     assert(node);
-    if (node->left == tree_nil) return;
+    if (node->left_ == tree_nil_) return;
 
     Node *&x = node;
 
-    Node *y = x->left;
+    Node *y = x->left_;
     // (b) right y subtree -> left x subtree
-    x->left = y->right;
+    x->left_ = y->right_;
 
     // b parent
-    if (y->right != tree_nil) {
-        y->right->parent = x;
+    if (y->right_ != tree_nil_) {
+        y->right_->parent = x;
     }
 
     // y parent
-    y->parent = x->parent;
+    y->parent_ = x->parent_;
 
     // x was tree root
-    if (x->parent == tree_nil) {
-        root = y;
+    if (x->parent_ == tree_nil_) {
+        root_ = y;
     // x was left subtree
-    } else if (x == x->parent->left) {
-        x->parent->left = y;
+    } else if (x == x->parent_->left) {
+        x->parent_->left = y;
     // x was right subtree
     } else {
-        x->parent->right = y;
+        x->parent_->right = y;
     }
 
-    y->right = x;
-    x->parent = y;
+    y->right_ = x;
+    x->parent_ = y;
 }
 
 }; // namespace RBTree
